9f391cc0f72a5992aa5ff596bf7eb2d847fdde3f
Mary Bucknell <mbucknell@usgs.gov>
2018-06-26 12:24:56 -0500

    Trying es

--- rollup.config.js
+++ rollup.config.js
@@ -41,7 +41,7 @@ const getBundleConfig = function(src, dest) {
         output: {
             name: 'wdfnviz_bundle',
             file: dest,
-            format: 'umd',
+            format: 'es',
             sourcemap: 'inline'
         }
     };
